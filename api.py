549d33915a6132e3eaf9f7db1fb9d0e0b870affd
Reema-Khaseeb <reemafkhaseeb@gmail.com>
2022-12-26 02:07:45 +0200

    Different combinations for cross validation

--- api.py
+++ api.py
@@ -8,24 +8,16 @@ import numpy as np
 import pandas as pd
 import uvicorn
 from fastapi import FastAPI, Request
-from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
 
 # Local application/library-specific imports
-from src.data_handler import Sample, rename_columns_dict
+from src.data_handler import Sample
 
 
-
-gbr_model_hp_pipeline = pickle.load(open('./pkls/best_model_gbr_random_search.pkl', 'rb'))
-
-
+model_pipeline = pickle.load(open('./pkls/best_model_gbt_random_search.pkl', 'rb'))
 
 # Define application
 app = FastAPI()
 
-# templates = Jinja2Templates(directory="templates")
-
-# app.mount("./Front-End/static", StaticFiles(directory="/Front-End/static"), name="static")
 
 # Check the connection is OK by initial path
 @app.get("/")
@@ -37,19 +29,12 @@ def _health_check() -> Dict:
     }
     return response
 
-# @app.get('/render_template')
-# def render_template(request: Request):
-#     return templates.TemplateResponse("predict-car-price.html", {
-#     "request": request,
-#     })
-
-
 @app.post("/predict/")
 def _predicted_price(sample: Sample) -> Dict:
     sample = [vars(sample)]
-    sample_dataframe = pd.DataFrame(sample) 
+    sample_dataframe = pd.DataFrame(sample)
     # sample_dataframe.rename(columns = rename_columns_dict, inplace=True)
-    predection = gbr_model_hp_pipeline.predict(sample_dataframe)[0]
+    predection = model_pipeline.predict(sample_dataframe)[0]
     response = {
         'prediction': predection
     }
